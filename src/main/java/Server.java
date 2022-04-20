5b29b01c1a0aa6812c17ddcdbce122a3f496e9d0
Artem Radchenko <irmate1987@mail.ru>
2022-04-20 13:45:13 +0300

    changes added

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,20 +1,98 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
+    private final int POOL_SIZE = 64;
+    private final ExecutorService threadPool;
+    private final List<String> validPaths = List.of(
+            "/index.html",
+            "/spring.svg",
+            "/spring.png",
+            "/resources.html",
+            "/styles.css",
+            "/app.js",
+            "/links.html",
+            "/forms.html",
+            "/classic.html",
+            "/events.html",
+            "/events.js"
+    );
 
-    private ServerSocket serverSocket;
+    public Server() {
+        threadPool = Executors.newFixedThreadPool(POOL_SIZE);
+    }
 
     public void listen(int port) {
-        try {
-            serverSocket = new ServerSocket(port);
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                threadPool.submit(() -> connect(socket));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public Socket toAccept() throws IOException {
-        return serverSocket.accept();
+    private void connect(Socket socket) {
+        try (var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             var out = new BufferedOutputStream(socket.getOutputStream())) {
+            while (true) {
+                final var requestLine = in.readLine();
+                final var parts = requestLine.split(" ");
+
+                if (parts.length != 3) {
+                    continue;
+                }
+                final var path = parts[1];
+                if (!validPaths.contains(path)) {
+                    out.write((
+                            "HTTP/1.1 404 Not Found\r\n" +
+                                    "Content-Length: 0\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.flush();
+                    continue;
+                }
+                final var filePath = Path.of(".", "public", path);
+                final var mimeType = Files.probeContentType(filePath);
+                if (path.equals("/classic.html")) {
+                    final var template = Files.readString(filePath);
+                    final var content = template.replace(
+                            "{time}",
+                            LocalDateTime.now().toString()
+                    ).getBytes();
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + content.length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.write(content);
+                    out.flush();
+                    continue;
+                }
+                final var length = Files.size(filePath);
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, out);
+                out.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
